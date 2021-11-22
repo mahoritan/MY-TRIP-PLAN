5ebcb7c902194748894fb4d7355a97c8a68bf771
mahoritan <papipupepon304@gmail.com>
2021-11-22 12:36:50 +0000

    address削除

--- db/schema.rb
+++ db/schema.rb
@@ -77,10 +77,10 @@ ActiveRecord::Schema.define(version: 2021_11_06_022254) do
     t.string "name"
     t.integer "gender"
     t.text "introduction"
-    t.string "address"
     t.string "image_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "prefecture_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
